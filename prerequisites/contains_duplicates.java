c02d1493f33b55e22b7e7f59832b09d4b789fffb
anant-jain01 <jain.anant609@gmail.com>
2025-07-13 17:05:54 +0530

    modified:   prerequisites/contains_duplicates.java

--- prerequisites/contains_duplicates.java
+++ prerequisites/contains_duplicates.java
@@ -1,8 +1,8 @@
 package prerequisites;
 
-import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Scanner;
-
+import java.util.Set;
 
 public class contains_duplicates {
   public static void main(String[] args) {
